01aac3a79e95d52c6fdb9c09bba6f19156c995fe
QuangVinh <vinhdqde170663@fpt.edu.vn>
2023-09-28 16:18:38 +0700

    Add project files.

---
+++ slot7/program7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slot7
+{
+    internal class program7
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "David", "Jane", "Peter", "John", "Mark" };
+            foreach (string name in names.OrderBy(s=>s)) {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
